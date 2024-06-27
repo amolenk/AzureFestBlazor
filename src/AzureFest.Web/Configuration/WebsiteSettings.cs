601575893031d0c0637399acec4f8d6f571935c9
Sander Molenkamp <sander@sandermolenkamp.com>
2024-06-27 08:54:32 +0200

    Open ticket sales automatically

--- src/AzureFest.Web/Configuration/WebsiteSettings.cs
+++ src/AzureFest.Web/Configuration/WebsiteSettings.cs
@@ -6,10 +6,13 @@ public class WebsiteSettings
     public bool SessionsAnnounced { get; init; }
     public bool ScheduleAnnounced { get; init; }
     public bool SpeakersAnnounced { get; init; }
-    public bool TicketsAvailable { get; init; }
+    public DateOnly? TicketsAvailableFromDate { get; init; }
     public string? SessionizeCfpLink { get; init; }
     public string? EventbriteLink { get; init; }
     public List<Sponsor> GoldSponsors { get; init; } = [];
     public List<Sponsor> CommunitySponsors { get; init; } = [];
     public List<Organizer> Organizers { get; init; } = [];
+    
+    public bool TicketsAvailable => TicketsAvailableFromDate.HasValue 
+                                    && TicketsAvailableFromDate.Value <= DateOnly.FromDateTime(DateTime.UtcNow);
 }
