6b2e686f65a6d2e34603dd368c42964b73ba99c6
Sander Molenkamp <sander@sandermolenkamp.com>
2026-02-19 17:16:39 +0100

    Fix

--- src-react/next.config.ts
+++ src-react/next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Required for App Service artifact deployments without running npm install on the host.
+  output: "standalone",
 
-    eslint: {
+  eslint: {
     // ⚠️ WARNING: This allows production builds to successfully complete
     // even if your project has ESLint errors.
     // TODO Fix the damn errors and remove this hack
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
 };
 
